3c85d54fc8afb39e378c95c1fe445b841b328351
Makhosandile <56253310+makhosi6@users.noreply.github.com>
2024-09-06 13:35:27 +0200

    hotfix/v2.9.0

---
+++ index.js
@@ -0,0 +1,14 @@
+import { __decorate } from "tslib";
+import { NgModule } from '@angular/core';
+import { SignaturePad } from './signature-pad';
+let SignaturePadModule = class SignaturePadModule {
+};
+SignaturePadModule = __decorate([
+    NgModule({
+        imports: [],
+        declarations: [SignaturePad],
+        exports: [SignaturePad],
+    })
+], SignaturePadModule);
+export { SignaturePadModule };
+//# sourceMappingURL=index.js.map
--- projects/angular2-signaturepad/src/lib/angular2-signaturepad.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SignaturePad } from './angular2-signaturepad.component';
-
-@NgModule({
-  declarations: [SignaturePad],
-  imports: [],
-  exports: [SignaturePad],
-})
-export class SignaturePadModule {}
